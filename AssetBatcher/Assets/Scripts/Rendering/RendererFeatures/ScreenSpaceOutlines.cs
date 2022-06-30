d4b84fd7c070f6b897c36e2fd52dbec70a3b5fa8
eungyukm <eungyukm@gmail.com>
2022-06-30 19:10:42 +0900

    upload

--- AssetBatcher/Assets/Scripts/Rendering/RendererFeatures/ScreenSpaceOutlines.cs
+++ AssetBatcher/Assets/Scripts/Rendering/RendererFeatures/ScreenSpaceOutlines.cs
@@ -11,7 +11,7 @@ public class ScreenSpaceOutlines : ScriptableRendererFeature
     {
         public RenderTextureFormat colorFormat;
         public int depthBufferBits;
-
+        public Color backgroundColor;
     }
     
     private class ViewSpaceNormalsTexturePss : ScriptableRenderPass
@@ -19,10 +19,14 @@ public class ScreenSpaceOutlines : ScriptableRendererFeature
         private ViewSpaceNormalsTextureSettings normalsTextureSettings;
         private readonly List<ShaderTagId> shaderTagIdList;
         private readonly RenderTargetHandle normals;
+        private readonly Material normalsMaterial;
+        
         
 
         public ViewSpaceNormalsTexturePss(RenderPassEvent renderPassEvent)
         {
+            normalsMaterial = new Material(
+                Shader.Find("Hidden/ViewSpaceNormalsShader"));
             this.renderPassEvent = renderPassEvent;
             normals.Init("_SceneViewSpaceNormals");
             shaderTagIdList = new List<ShaderTagId>
@@ -48,6 +52,10 @@ public class ScreenSpaceOutlines : ScriptableRendererFeature
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
+            if (!normalsMaterial)
+            {
+                return;
+            }
             CommandBuffer cmd = CommandBufferPool.Get();
             using (new ProfilingScope(cmd, new ProfilingSampler(
                        "SceneViewSpaceNormalsTextureCreation")))
@@ -58,10 +66,12 @@ public class ScreenSpaceOutlines : ScriptableRendererFeature
                 DrawingSettings drawSettings = CreateDrawingSettings(
                     shaderTagIdList, ref renderingData,
                     renderingData.cameraData.defaultOpaqueSortFlags);
+                drawSettings.overrideMaterial = normalsMaterial;
                 FilteringSettings filteringSettings = FilteringSettings.defaultValue;
                 context.DrawRenderers(renderingData.cullResults,
                         ref drawSettings, ref filteringSettings);
-                )
+                CommandBufferPool.Release(cmd);
+                
             }
         }
 
