2ab310efb43df1f4aa3df534d20c1e27ea96bcc1
eungyukm <eungyukm@gmail.com>
2022-07-21 10:45:28 +0900

    프로젝트 정리

--- AssetBatcher/Assets/Scripts/Managers/WorldObjectsManager.cs
+++ AssetBatcher/Assets/Scripts/Managers/WorldObjectsManager.cs
@@ -1,9 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldObjectsManager : MonoBehaviour
 {
+    public static WorldObjectsManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+
     public List<GameObject> worldObjects{ set; get; }
 
     public GameObject[] GameObjects;
